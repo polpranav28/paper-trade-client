bfd779e0bed97867efb19adedc7095c7f8a2e82a
Abhishek Ramane <35802001+abhishek-ramane@users.noreply.github.com>
2021-07-27 20:57:15 +0530

    git demo changes

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { StatsComponent } from './components/stats/stats.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { CommonNgZorroAntdModule } from 'src/app/ng-zorro-antd.module';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   declarations: [StatsComponent],
-  imports: [CommonModule, DashboardRoutingModule, CommonNgZorroAntdModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    CommonNgZorroAntdModule,
+    NzIconModule,
+  ],
 })
 export class DashboardModule {}
